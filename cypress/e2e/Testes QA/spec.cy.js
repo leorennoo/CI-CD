17b1a1a3fa4e24564b203e1ec31a3fa6b7f9a86a
Leonardo Rennó <leonardogrenno@hotmail.com>
2024-08-20 04:53:09 -0300

    teste de login

--- cypress/e2e/Testes QA/spec.cy.js
+++ cypress/e2e/Testes QA/spec.cy.js
@@ -2,7 +2,7 @@ describe('template spec', () => {
   // LOGIN INVALIDO
   it('login invalido', () => {
     cy.visit('https://front.serverest.dev/login')
-    cy.get('[data-testid="email"]').type('testeleo2@hotmail.com')
+    cy.get('[data-testid="email"]').type('testeleo3@hotmail.com')
     cy.get('[data-testid="senha"]').type('teste1234')
     cy.get('[data-testid="entrar"]').click()
     cy.contains('Email e/ou senha inválidos').should('exist')
@@ -13,10 +13,19 @@ describe('template spec', () => {
     cy.visit('https://front.serverest.dev/login')
     cy.get('[data-testid="cadastrar"]').click()
     cy.get('[data-testid="nome"]').type('leo')
-    cy.get('[data-testid="email"]').type('testeleo2@hotmail.com')
+    cy.get('[data-testid="email"]').type('testeleo4@hotmail.com')
     cy.get('[data-testid="password"]').type('teste1234')
     cy.get('[data-testid="checkbox"]').click()
     cy.get('[data-testid="cadastrar"]').click()
     cy.get('.alert').contains('Cadastro realizado com sucesso')
+  })
+
+  //ACESSAR CONTA CADASTRADA
+  it.only ('cadastrar usuário', () => {
+    cy.visit('https://front.serverest.dev/login')
+    cy.get('[data-testid="email"]').type('testeleo4@hotmail.com')
+    cy.get('[data-testid="senha"]').type('teste1234')
+    cy.get('[data-testid="entrar"]').click()
+    cy.get('h1').contains('Bem Vindo leo')
   })
 })
